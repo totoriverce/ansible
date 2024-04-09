8d4e15f529d93d61c05be5f8e836b5605224659a
Brian Coca <brian.coca+git@gmail.com>
2024-04-09 14:39:58 -0400

    moar tests

--- test/integration/targets/deprecations/runme.sh
+++ test/integration/targets/deprecations/runme.sh
@@ -3,14 +3,18 @@
 set -eux -o pipefail
 
 # check for entry key valid, no deprecation
-[ "$(ANSIBLE_CONFIG=entry_key_not_deprecated.cfg ansible -m meta -a 'noop'  localhost 2>&1 | grep -c 'DEPRECATION')" -eq "0" ]
+[ "$(ANSIBLE_CONFIG='entry_key_not_deprecated.cfg' ansible -m meta -a 'noop'  localhost 2>&1 | grep -c 'DEPRECATION')" -eq "0" ]
 
 # check for entry key deprecation, must be defined to trigger
-[ "$(ANSIBLE_CONFIG=entry_key_deprecated.cfg ansible -m meta -a 'noop'  localhost 2>&1 | grep -c 'DEPRECATION')" -eq "1" ]
+[ "$(ANSIBLE_CONFIG='entry_key_deprecated.cfg' ansible -m meta -a 'noop'  localhost 2>&1 | grep -c 'DEPRECATION')" -eq "1" ]
 
 # check for deprecation of entry itself, must be consumed to trigger
 [ "$(ANSIBLE_TEST_ENTRY2=1 ansible -m debug -a 'msg={{q("config", "_Z_TEST_ENTRY_2")}}' localhost  2>&1 | grep -c 'DEPRECATION')" -eq "1" ]
 
+# check for entry deprecation, just need key defined to trigger
+[ "$(ANSIBLE_CONFIG='entry_key_deprecated2.cfg' ansible -m meta -a 'noop'  localhost 2>&1 | grep -c 'DEPRECATION')" -eq "1" ]
+
+# check for deprecation of entry itself, must be consumed to trigger
 # TODO: check for module deprecation
 # TODO: check for module option deprecation
 # TODO: check for plugin(s) deprecation
