f28bf84c3d43aa0b1e7ed1435ec87970c95bb893
Brian Coca <brian.coca+git@gmail.com>
2024-04-09 19:23:13 -0400

    moar tests

--- test/integration/targets/deprecations/runme.sh
+++ test/integration/targets/deprecations/runme.sh
@@ -4,6 +4,8 @@ set -eux -o pipefail
 
 export ANSIBLE_DEPRECATION_WARNINGS=True
 
+### check general config
+
 # check for entry key valid, no deprecation
 [ "$(ANSIBLE_CONFIG='entry_key_not_deprecated.cfg' ansible -m meta -a 'noop'  localhost 2>&1 | grep -c 'DEPRECATION')" -eq "0" ]
 
@@ -16,9 +18,22 @@ export ANSIBLE_DEPRECATION_WARNINGS=True
 # check for entry deprecation, just need key defined to trigger
 [ "$(ANSIBLE_CONFIG='entry_key_deprecated2.cfg' ansible -m meta -a 'noop'  localhost 2>&1 | grep -c 'DEPRECATION')" -eq "1" ]
 
-# check for deprecation of entry itself, must be consumed to trigger
+
+### check plugin config
+
+# force use of the test plugin
+export ANSIBLE_CACHE_PLUGIN_CONNECTION=/var/tmp
+export ANSIBLE_CACHE_PLUGIN=notjsonfile
+
+# check for plugin(s) config option and setting non deprecation
+[ "$(ANSIBLE_CACHE_PLUGIN_TIMEOUT=1 ansible -m meta -a 'noop'  localhost --playbook-dir ./ 2>&1 | grep -c 'DEPRECATION')" -eq "0" ]
+
+# check for plugin(s) config option setting deprecation
+[ "$(ANSIBLE_NOTJSON_CACHE_PLUGIN_TIMEOUT=1 ansible -m meta -a 'noop'  localhost --playbook-dir ./ 2>&1 | grep -c 'DEPRECATION')" -eq "1" ]
+
+# check for plugin(s) config option deprecation
+[ "$(ANSIBLE_NOTJSON_CACHE_PLUGIN_REMOVEME=1 ansible -m meta -a 'noop'  localhost --playbook-dir ./ 2>&1 | grep -c 'DEPRECATION')" -eq "1" ]
+
 # TODO: check for module deprecation
 # TODO: check for module option deprecation
-# TODO: check for plugin(s) deprecation
-# TODO: check for plugin(s) config option deprecation
-# TODO: check for plugin(s) config option setting path deprecation
+# TODO: check for plugin deprecation
